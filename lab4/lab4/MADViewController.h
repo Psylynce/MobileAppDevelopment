424671cf11840df71d882928e24ec4c3e024e33f
Justin Richard Powell <jupo3737@atls221-136-dhcp.colorado.edu>
2013-09-17 10:21:54 -0600

    Final Lab4

--- lab4/lab4/MADViewController.h
+++ lab4/lab4/MADViewController.h
@@ -9,5 +9,13 @@
 #import <UIKit/UIKit.h>
 
 @interface MADViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UIImageView *beatlesImage;
+@property (weak, nonatomic) IBOutlet UILabel *titleLabel;
+@property (weak, nonatomic) IBOutlet UISegmentedControl *imageControl;
+- (IBAction)changeImage:(UISegmentedControl *)sender;
+@property (weak, nonatomic) IBOutlet UISwitch *capitalizeSwitch;
+- (IBAction)changeSwitch:(UISwitch *)sender;
+@property (weak, nonatomic) IBOutlet UILabel *fontSizeLabel;
+- (IBAction)changeFontSize:(UISlider *)sender;
 
 @end
--- lab4/lab4/MADViewController.m
+++ lab4/lab4/MADViewController.m
@@ -16,6 +16,7 @@
 
 - (void)viewDidLoad
 {
+    _imageControl.selectedSegmentIndex = -1;
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
 }
@@ -25,5 +26,40 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+-(void)updateImage{
+    if (_imageControl.selectedSegmentIndex == 0) {
+        _titleLabel.text = @"Young Beatles";
+        _beatlesImage.image = [UIImage imageNamed:@"beatles1.png"];
+    }
+    else{
+        _titleLabel.text = @"Experienced Beatles";
+        _beatlesImage.image = [UIImage imageNamed:@"beatles2.png"];
+    }
+}
+
+-(void)updateCaps{
+    if (_capitalizeSwitch.on) {
+        _titleLabel.text = [_titleLabel.text uppercaseString];
+    }
+    else{
+        _titleLabel.text = [_titleLabel.text lowercaseString];
+    }
+}
+
+- (IBAction)changeImage:(UISegmentedControl *)sender {
+    [self updateImage];
+    [self updateCaps];
+}
+- (IBAction)changeSwitch:(UISwitch *)sender {
+    [self updateCaps];
+}
+- (IBAction)changeFontSize:(UISlider *)sender {
+    int fontSize = sender.value;
+    _fontSizeLabel.text = [NSString stringWithFormat:@"%d", fontSize];
+    UIFont *newFont = [UIFont systemFontOfSize:sender.value];
+    _titleLabel.font = newFont;
+}
+
 
 @end
