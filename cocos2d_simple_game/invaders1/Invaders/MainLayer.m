3b446f6132b1020eac2aaef89ed728a5f1117ca3
Psylynce <justin.powell@colorado.edu>
2013-11-12 10:46:20 -0700

    Cocos2d tutorial

---
+++ cocos2d_simple_game/invaders1/Invaders/MainLayer.m
@@ -0,0 +1,42 @@
+//
+//  MainLayer.m
+//  Invaders1
+//
+
+#import "MainLayer.h"
+#import "AppDelegate.h"
+
+@implementation MainLayer {
+    CCSprite *alien;
+}
+
++(CCScene *) scene {
+    CCScene *scene = [CCScene node];
+    MainLayer *layer = [MainLayer node];
+    [scene addChild: layer];
+    return scene;
+}
+
+-(id) init {
+    if( (self=[super init]) ) {
+        alien = [CCSprite spriteWithFile:@"alien.png"];
+
+        CGSize size = [[CCDirector sharedDirector] winSize];
+        alien.position = ccp(size.width/2, size.height/2);
+
+        [self addChild:alien];
+
+        [self scheduleUpdate];
+    }
+    return self;
+}
+
+-(void) update:(ccTime)delta {
+    alien.position = ccpAdd(ccp(1,1), alien.position);
+    alien.rotation += 1.0;
+    alien.scale += 0.3;
+    //alien.rotation (in degrees)
+    //alien.scale (in percent)
+}
+
+@end
---
+++ cocos2d_simple_game/invaders1/Invaders/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  Invaders1
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    @autoreleasepool {
+        int retVal = UIApplicationMain(argc, argv, nil, @"AppController");
+        return retVal;
+    }
+}
