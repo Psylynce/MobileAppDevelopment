7af85952f7d9ace519ff8b81c501390da67b8297
Psylynce <justin.powell@colorado.edu>
2013-12-08 09:59:32 -0700

    Empty Cocos2D Project

---
+++ Pong/Pong/IntroLayer.h
@@ -0,0 +1,21 @@
+//
+//  IntroLayer.h
+//  Pong
+//
+//  Created by Justin Powell on 12/8/13.
+//  Copyright Justin Powell 2013. All rights reserved.
+//
+
+
+// When you import this file, you import all the cocos2d classes
+#import "cocos2d.h"
+
+// HelloWorldLayer
+@interface IntroLayer : CCLayer
+{
+}
+
+// returns a CCScene that contains the HelloWorldLayer as the only child
++(CCScene *) scene;
+
+@end
---
+++ Pong/Pong/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Pong
+//
+//  Created by Justin Powell on 12/8/13.
+//  Copyright Justin Powell 2013. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppController");
+    [pool release];
+    return retVal;
+}
